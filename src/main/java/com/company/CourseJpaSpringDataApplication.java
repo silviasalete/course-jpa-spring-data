c723b48a2640d1b22624e6d3b398b4bcd25644cd
Silvia <silvia.samtech@gmal.com>
2021-03-22 11:25:58 -0300

    :tada: First Commit to Master

---
+++ src/main/java/com/company/CourseJpaSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJpaSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJpaSpringDataApplication.class, args);
+	}
+
+}
